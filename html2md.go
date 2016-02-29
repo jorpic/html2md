c29a90945f84c498606c7a76adfda42602e4dc05
Max Taldykin <jorpic@gmail.com>
2016-02-29 23:37:18 +0300

    go fmt

--- html2md.go
+++ html2md.go
@@ -1,129 +1,122 @@
 package main
 
 import (
-  "os"
-  "io"
-  "golang.org/x/net/html"
-  "golang.org/x/net/html/atom"
-  "strings"
-  "bytes"
+	"bytes"
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
+	"io"
+	"os"
+	"strings"
 )
 
-
 func main() {
-  html2md(os.Stdin, os.Stdout)
+	html2md(os.Stdin, os.Stdout)
 }
 
-
 type context struct {
-  stack     []atom.Atom
-  tokenizer *html.Tokenizer
-  writer    io.Writer
-  tagTable  map[atom.Atom]dispatcher
+	stack     []atom.Atom
+	tokenizer *html.Tokenizer
+	writer    io.Writer
+	tagTable  map[atom.Atom]dispatcher
 }
 
 type parserFunc func(context)
 
 type dispatcher struct {
-  parser parserFunc
-  nestedTable map[atom.Atom]dispatcher
+	parser      parserFunc
+	nestedTable map[atom.Atom]dispatcher
 }
 
-
 var textTable = map[atom.Atom]dispatcher{
-    0: {text, nil},
-  }
+	0: {text, nil},
+}
 
 var bodyTable = map[atom.Atom]dispatcher{
-    0:       {text, nil},
-    atom.H1: {h1, textTable},
-    atom.H2: {h2, textTable},
-    atom.H5: {h5, textTable},
-  }
-
+	0:       {text, nil},
+	atom.H1: {h1, textTable},
+	atom.H2: {h2, textTable},
+	atom.H5: {h5, textTable},
+}
 
 func html2md(r io.Reader, w io.Writer) {
-  z := html.NewTokenizer(r)
-  cxt := context{
-    tokenizer: z,
-    writer: w,
-    tagTable: bodyTable,}
+	z := html.NewTokenizer(r)
+	cxt := context{
+		tokenizer: z,
+		writer:    w,
+		tagTable:  bodyTable}
 
-  for dispatch(cxt) {
-  }
+	for dispatch(cxt) {
+	}
 }
 
 func dispatch(cxt context) bool {
-  switch cxt.tokenizer.Next() {
-  case html.ErrorToken:
-    return false
-  case html.StartTagToken:
-    tag := cxt.tokenizer.Token().DataAtom
-    d, ok := cxt.tagTable[tag]
-    if ok {
-      newCxt := cxt
-      newCxt.tagTable = d.nestedTable
-      d.parser(newCxt)
-    }
-  case html.TextToken:
-    d := cxt.tagTable[0]
-    newCxt := cxt
-    newCxt.tagTable = d.nestedTable
-    d.parser(newCxt)
-  case html.SelfClosingTagToken:
-    // br
-  default:
-  }
-  return true
+	switch cxt.tokenizer.Next() {
+	case html.ErrorToken:
+		return false
+	case html.StartTagToken:
+		tag := cxt.tokenizer.Token().DataAtom
+		d, ok := cxt.tagTable[tag]
+		if ok {
+			newCxt := cxt
+			newCxt.tagTable = d.nestedTable
+			d.parser(newCxt)
+		}
+	case html.TextToken:
+		d := cxt.tagTable[0]
+		newCxt := cxt
+		newCxt.tagTable = d.nestedTable
+		d.parser(newCxt)
+	case html.SelfClosingTagToken:
+		// br
+	default:
+	}
+	return true
 }
-
 
 func text(cxt context) {
-  cxt.writer.Write(cxt.tokenizer.Text())
+	cxt.writer.Write(cxt.tokenizer.Text())
 }
 
-
 func h1(cxt context) {
-  w   := cxt.writer
+	w := cxt.writer
 
-  buf := new(bytes.Buffer)
-  newCxt := cxt
-  newCxt.writer = buf
-  dispatch(newCxt)
-  txt := buf.String()
+	buf := new(bytes.Buffer)
+	newCxt := cxt
+	newCxt.writer = buf
+	dispatch(newCxt)
+	txt := buf.String()
 
-  io.WriteString(w, "\n")
-  io.WriteString(w, txt)
-  io.WriteString(w, "\n")
-  io.WriteString(w, strings.Repeat("=", len(txt)))
-  io.WriteString(w, "\n")
+	io.WriteString(w, "\n")
+	io.WriteString(w, txt)
+	io.WriteString(w, "\n")
+	io.WriteString(w, strings.Repeat("=", len(txt)))
+	io.WriteString(w, "\n")
 }
 
 func h2(cxt context) {
-  buf := new(bytes.Buffer)
-  newCxt := cxt
-  newCxt.writer = buf
-  dispatch(newCxt)
-  txt := buf.String()
+	buf := new(bytes.Buffer)
+	newCxt := cxt
+	newCxt.writer = buf
+	dispatch(newCxt)
+	txt := buf.String()
 
-  w   := cxt.writer
-  io.WriteString(w, "\n")
-  io.WriteString(w, txt)
-  io.WriteString(w, "\n")
-  io.WriteString(w, strings.Repeat("-", len(txt)))
-  io.WriteString(w, "\n")
+	w := cxt.writer
+	io.WriteString(w, "\n")
+	io.WriteString(w, txt)
+	io.WriteString(w, "\n")
+	io.WriteString(w, strings.Repeat("-", len(txt)))
+	io.WriteString(w, "\n")
 }
 
 func h5(cxt context) {
-  buf := new(bytes.Buffer)
-  newCxt := cxt
-  newCxt.writer = buf
-  dispatch(newCxt)
-  txt := buf.String()
+	buf := new(bytes.Buffer)
+	newCxt := cxt
+	newCxt.writer = buf
+	dispatch(newCxt)
+	txt := buf.String()
 
-  w := cxt.writer
-  io.WriteString(w, "\n##### ")
-  io.WriteString(w, txt)
-  io.WriteString(w, "\n")
+	w := cxt.writer
+	io.WriteString(w, "\n##### ")
+	io.WriteString(w, txt)
+	io.WriteString(w, "\n")
 }
-
--- html2md_test.go
+++ html2md_test.go
@@ -1,40 +1,38 @@
 package main
 
 import (
-  t "testing"
-  "strings"
-  "bytes"
+	"bytes"
+	"strings"
+	t "testing"
 )
 
-
 func TestText(t *t.T) {
-  check(t, "some text", "some text")
+	check(t, "some text", "some text")
 }
 
 func TestH1(t *t.T) {
-  check(t,
-    "<h1>Hello!</h1>",
-    "\nHello!\n======\n")
+	check(t,
+		"<h1>Hello!</h1>",
+		"\nHello!\n======\n")
 }
 
 func TestH2(t *t.T) {
-  check(t,
-    "<h2>Hello!</h2>",
-    "\nHello!\n------\n")
+	check(t,
+		"<h2>Hello!</h2>",
+		"\nHello!\n------\n")
 }
 
 func TestH5(t *t.T) {
-  check(t,
-    "<h5>Hello!</h5>",
-    "\n##### Hello!\n")
+	check(t,
+		"<h5>Hello!</h5>",
+		"\n##### Hello!\n")
 }
 
-
 func check(t *t.T, in, out string) {
-  w := new(bytes.Buffer)
-  html2md(strings.NewReader(in), w)
-  res := w.String()
-  if out != res {
-    t.Errorf("\n%s\n != \n%s", res, out)
-  }
+	w := new(bytes.Buffer)
+	html2md(strings.NewReader(in), w)
+	res := w.String()
+	if out != res {
+		t.Errorf("\n%s\n != \n%s", res, out)
+	}
 }
